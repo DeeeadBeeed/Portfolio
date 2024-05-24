4f5e616767838398cc4b5865935a891b716fe7e7
DeeeadBeeed <rhythmic34u@gmail.com>
2024-05-24 15:51:20 +0800

    Update file with new links to open in another tab

--- src/app/MainSection.tsx
+++ src/app/MainSection.tsx
@@ -3,15 +3,18 @@ import './glow.css';
 
 export default function MainSection() {
   return (
-    <div className='min-h-screen flex items-center justify-between lg:flex-row'>
-      <div>
-        <h1 className='text-4xl lg:text-7xl font-bold'>Nice To Meet you!<br/><span>{"I am Oakar"}</span><br/></h1>
-        <div className='flex'>
+    <div className='min-h-screen flex flex-col items-center justify-center lg:flex-row'>
+      <div className="text-center lg:text-left">
+        <h1 className='text-2xl lg:text-7xl font-bold'>Nice To Meet you!<br/><span>{"I am Oakar"}</span><br/></h1>
+        <p className='mt-10 mb-5'>
+        Welcome to my portfolio! I specialize in React and can also help with backend development using PostgreSQL, MongoDB, REST APIs, and Node.js. Explore my work, and let's connect to discuss how I can bring your next project to life. 
+</p>
+        <div className='flex flex-col items-center lg:flex-row lg:items-start'>
           <a href="mailto:rhythmic34u@gmail.com">
-            <h1 className='underline underline-offset-8 inline-block text-3xl mt-5'>✉️ Contact Me</h1>
+            <h1 className='underline underline-offset-8 inline-block text-xl lg:text-3xl mt-5'>✉️ Contact Me</h1>
           </a>
-          <a href="https://drive.google.com/file/d/17wIZ12vbwNdyEDnMDiyg7WbaiQQqzJpk/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
-            <button className='button ml-10 mt-4'>
+          <a href="https://drive.google.com/file/d/17wIZ12vbwNdyEDnMDiyg7WbaiQQqzJpk/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className='lg:ml-10 mt-4'>
+            <button className='button'>
               Download CV
             </button>
           </a>
--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import { SiLinkedin,SiGithub} from 'react-icons/si';
+import { MdEmail } from "react-icons/md";
 export default function Navbar() {
   return (
     <nav className='py-5 md:py-10 flex flex-col md:flex-row justify-center md:justify-between items-center'>
       <h1 className='text-lg md:text-2xl font-bold underline underline-offset-8 decoration-green-500 text-center md:text-left mb-5 md:mb-0'>Full-stack / Front-end Developer</h1>
       <div className='flex items-center gap-5'>
-        <a href="" className='block md:inline-block'>LinkedIn</a>
-        <a href="" className='block md:inline-block'>GitHub</a>
-        <a href="" className='block md:inline-block'>Email</a>
-      </div>
+    <a href="https://www.linkedin.com/in/mg-oakar-147474255/" className='block md:inline-block' target="_blank" rel="noopener noreferrer"><SiLinkedin /></a>
+    <a href="https://github.com/DeeeadBeeed" className='block md:inline-block' target="_blank" rel="noopener noreferrer"><SiGithub /></a>
+    <a href="mailto:rhythmic34u@gmail.com" className='block md:inline-block' target="_blank" rel="noopener noreferrer"><MdEmail /></a>
+</div>
+
+
     </nav>
   );
 }

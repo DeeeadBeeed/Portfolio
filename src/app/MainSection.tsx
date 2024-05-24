e1e09b4a02abc06382ac508ffba481712a21abfe
DeeeadBeeed <rhythmic34u@gmail.com>
2024-05-24 15:57:14 +0800

    updated

--- src/app/MainSection.tsx
+++ src/app/MainSection.tsx
@@ -7,8 +7,8 @@ export default function MainSection() {
       <div className="text-center lg:text-left">
         <h1 className='text-2xl lg:text-7xl font-bold'>Nice To Meet you!<br/><span>{"I am Oakar"}</span><br/></h1>
         <p className='mt-10 mb-5'>
-        Welcome to my portfolio! I specialize in React and can also help with backend development using PostgreSQL, MongoDB, REST APIs, and Node.js. Explore my work, and let's connect to discuss how I can bring your next project to life. 
-</p>
+        Welcome to my portfolio! I specialize in React and can also help with backend development using PostgreSQL, MongoDB, REST APIs, and Node.js. Explore my work, and let&apos;s connect to discuss how I can bring your next project to life. 
+        </p>
         <div className='flex flex-col items-center lg:flex-row lg:items-start'>
           <a href="mailto:rhythmic34u@gmail.com">
             <h1 className='underline underline-offset-8 inline-block text-xl lg:text-3xl mt-5'>✉️ Contact Me</h1>
